5c142f0362592ebd8950daea67fbb77a6fe762d2
unknown <iamjui2010@hotmail.com>
2019-05-02 20:05:29 -0700

    allcode

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -2,11 +2,27 @@
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
+#define R D0
+#define G D1
+#define B D2
+#define buzzerPIN D3
+#define LED7 D4
+#define clkLED7 D5
+#define backLEDPIN D7
+#define frontLEDPIN D8
+
 const char *ssid = "room1221";  //ENTER YOUR WIFI SETTINGS
 const char *password = "juiicepp";
-const char *host = "http://192.168.1.25:5000/jui";
+const char *host = "http://192.168.1.25:5000/train";
+
 void setup() {
-  pinMode(D0, OUTPUT);
+  pinMode(R, OUTPUT);
+  pinMode(G,OUTPUT);
+  pinMode(B,OUTPUT);
+  pinMode(buzzerPIN,OUTPUT);
+  pinMode(backLEDPIN,OUTPUT);
+  pinMode(frontLEDPIN,OUTPUT);
+
   Serial.begin(9600);
   delay(1000);
   WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
@@ -40,12 +56,36 @@ void loop() {
     Serial.println(httpCode);   //Print HTTP return code
     Serial.println(payload);    //Print request response payload
     http.end();  //Close connection
-    
-    if(payload[0]=='1'){
-      digitalWrite(D0, HIGH);   // turn the LED on (HIGH is the voltage level)    
+
+    char movement=payload[0];
+    char source=payload[2];
+    char dest=payload[3];
+    char horn=payload[1];
+ 
+    if(movement=='0'){
+      digitalWrite(frontLEDPIN, LOW);
+      digitalWrite(backLEDPIN, LOW);
+      Serial.println("Stopping");
     }
-    else{
-      digitalWrite(D0, LOW);    
+    else if(movement=='1'){
+      digitalWrite(frontLEDPIN, HIGH);
+      digitalWrite(backLEDPIN, LOW);
+      Serial.println("Forward");
+    }
+    else if(movement=='2'){
+      digitalWrite(frontLEDPIN, LOW);
+      digitalWrite(backLEDPIN, HIGH);
+      Serial.println("Reversed");
+    }
+
+
+    if(horn=='1'){
+      tone(buzzerPIN,100);
+      Serial.println("Beep !");
+    }
+    else if(horn=='0'){
+      tone(buzzerPIN,0);
+      Serial.println("Sleep Zzz...");
     }
     delay(1000);
    } 
